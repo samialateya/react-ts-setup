0e1f979c54949dea74c0fdb18f33625347417eca
sami <samialateya@hotmail.com>
2025-05-28 09:16:06 +0400

    fixes

--- src/lib/components/atoms/button/index.tsx
+++ src/lib/components/atoms/button/index.tsx
@@ -1,8 +1,13 @@
+import clsx from 'clsx';
+
 export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       {...props}
-      className={`bg-secondary inline-block cursor-pointer rounded px-4 py-2 text-white hover:opacity-95 ${props.className}`}
+      className={clsx(
+        'bg-secondary focus:ring-secondary focus:ring-opacity-50 inline-block cursor-pointer rounded px-4 py-2 text-white hover:opacity-95 focus:ring-2',
+        props.className ?? ''
+      )}
     >
       {props.children}
     </button>
